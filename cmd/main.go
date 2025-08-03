fec531d62653635fb2eb8c34165f57c0bd63d0d9
Kaz <ali@alikazai.com>
2025-08-03 21:18:08 +0100

    setup env and safe fiber shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/alikazai/standup-logger-app/utils"
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	fmt.Println("hello world")
+	app := fiber.New()
+
+	// ======================================
+	go func() {
+		if err := app.Listen(utils.GetHTTPListenAddress()); err != nil {
+			log.Panic().Err(err).Msg("Fiber server errror")
+		}
+	}()
+
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info().Msg("shutting down fiber...")
+
+	if err := app.Shutdown(); err != nil {
+		log.Panic().Err(err).Msg("Failed to shut down fiber gracefully")
+	}
+
+	log.Info().Msg("Fiber shut down cleanly")
 }
 
 func init() {
