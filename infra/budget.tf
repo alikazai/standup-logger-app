c9131bed0d048810c55cd6b04ec5d5805009b982
Kaz <ali@alikazai.com>
2025-08-11 21:30:57 +0100

    updated budget

--- infra/budget.tf
+++ infra/budget.tf
@@ -2,7 +2,7 @@ resource "aws_budgets_budget" "monthly" {
   name         = "standup-monthly-budget"
   budget_type  = "COST"
   limit_amount = "20"
-  limit_unit   = "GBP"
+  limit_unit   = "USD"
   time_unit    = "MONTHLY"
   notification {
     comparison_operator        = "GREATER_THAN"
